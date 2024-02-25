45e02888257c7f8d8298f5baeab4fa7ca8a3ccc6
mahesh ubale <maheshubale9244@gmail.com>
2024-02-25 20:41:11 +0530

    change css

--- src/app/topmovie/page.js
+++ src/app/topmovie/page.js
@@ -1,7 +1,7 @@
 "use client"
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
-import LoadingAnimation from "../loading";
+
 
 function Moviedata() {
   const [movielist, setMovielist] = useState([]);
@@ -34,51 +34,44 @@ function Moviedata() {
 
   return (
     <div>
-      {loading ? (
-        <LoadingAnimation />
-      ) : (
-        <>
-          <h2 className="text-center p-3" style={{ color: "white" }}>
-            Top Movies
-          </h2>
-          {movielist.slice(0, 5).map((movie) => (
-            <div
-              key={movie.id}
-              className="container my-3 p-3 d-flex"
-              style={{ color: "white" }}
-            >
-              <div
-                className="card mb-3 bs-tertiary-color-rgb "
-                style={{
-                  maxWidth: "340px",
-                  backgroundColor: "rgb(63, 61, 61)",
-                  color: "white",
-                }}
-              >
-                <Link href={`/topmovie/${movie.id}`}>
-                  <div className="row">
-                    <div className="col justify-content-md-center">
-                      <img
-                        src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-                        style={{ height: "60vh", width: "100%" }}
-                        className="rounded float-left img-fluid-sx"
-                        alt="..."
-                      />
-                    </div>
-                    <div className="col-md-9 " style={{}}>
-                      <div className="card-body">
-                        <h5 className="card-title grid gap-3">{movie.title}</h5>
-                        <h5>Overview :</h5>
-                        <p className="card-text"> {movie.overview}</p>
-                      </div>
-                    </div>
+      <h2 className="text-center p-3" style={{color:'white'}}>Top Movies</h2>
+      {movielist.slice(0, 5).map((movie) => (
+        <div
+          key={movie.id}
+          className="container my-3 p-3 d-flex"
+          style={{ color: "white" }}
+        >
+          <div
+            className="card mb-3 bs-tertiary-color-rgb "
+            style={{
+              maxwidth: " 340px",
+              backgroundColor: "rgb(63, 61, 61)",
+              color: "white",
+            }}
+          >
+            <Link href={`/topmovie/${movie.id}`}>
+              <div className="row">
+                <div className="col justify-content-md-center">
+                  <img
+                  
+                    src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                    style={{ height: "60vh", width: "100%" }}
+                    className="rounded float-left img-fluid-sx"
+                    alt="..."
+                  />
+                </div>
+                <div className="col-md-9 " style={{}}>
+                  <div className="card-body">
+                    <h5 className="card-title grid gap-3">{movie.title} </h5>
+                    <h5>Overview:</h5>
+                    <p className="card-text"> {movie.overview}</p>
                   </div>
-                </Link>
+                </div>
               </div>
-            </div>
-          ))}
-        </>
-      )}
+            </Link>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
